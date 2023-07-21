f080d6e2c7064ab6bbff552c6f5e3e561d21894d
Olena1010 <118633780+Olena1010@users.noreply.github.com>
2023-07-21 22:53:36 +0000

    Deploying to gh-pages from @ Olena1010/goit-react-hw-08-phonebook@e41580a7afe4afab45fb046f2a8ee4bea8c94682 🚀

--- static/js/75.3d936b3d.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkgoit_react_hw_08_phonebook=self.webpackChunkgoit_react_hw_08_phonebook||[]).push([[75],{6075:function(e,t,n){n.r(t),n.d(t,{default:function(){return r}});var i=n(5453),o=n(184),c={container:{minHeight:"calc(100vh - 50px)",display:"flex",alignItems:"center",justifyContent:"center"},title:{fontWeight:500,fontSize:48,textAlign:"center"}};function r(){return(0,o.jsxs)("div",{style:c.container,children:[(0,o.jsx)("h1",{style:c.title,children:"Phonebook welcome page"}),(0,o.jsx)(i.W3,{})]})}}}]);
-//# sourceMappingURL=75.3d936b3d.chunk.js.map
